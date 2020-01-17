e3357a675f788b2183bc1e3fd08a6ce7cd63c5f0
vidhi2296 <vidhipansuriya@gmail.com>
2020-01-17 14:24:04 -0500

    second commit

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -12,14 +12,37 @@ package ca.sheridancollege.week1.softwarefundamentals;
 public class Student {
     
     private String name;
+    private int Id;
+    private int age;
+    private String email;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 
     /**
      * Get the value of name
      *
      * @return the value of name
      */
-    public String getName() {
-        return name;
+    public void setEmail(String email) {    
+        this.email = email;
     }
 
     /**
