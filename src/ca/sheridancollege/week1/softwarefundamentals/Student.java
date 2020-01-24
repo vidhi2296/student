6e40318d9538846983ac200cb10d8d56fb25133d
vidhi2296 <59622348+vidhi2296@users.noreply.github.com>
2020-01-24 13:13:56 -0500

    Update Student.java

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -2,6 +2,8 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ 
+ vidhi pansuriya
  */
 package ca.sheridancollege.week1.softwarefundamentals;
 
